183d2c2d40570a1415290bfb315333be73119972
cd documents <cls>
2015-10-29 15:04:17 -0500

    View Matrícula y estilos

--- Views/matricula.php
+++ Views/matricula.php
@@ -3,30 +3,24 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Matricula</title>
+	<title>Matrícula</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet"  href="../css/normalize.css">
-	<link rel="stylesheet" href="../css/estilos.css">
-	<link rel="stylesheet" href="../css/estiloscourses.css">
+	<link rel="stylesheet" href="../css/estilosmatri.css">
 	<link href='https://fonts.googleapis.com/css?family=Montserrat+Alternates' rel='stylesheet' type='text/css'>
 	<link rel="icon" type="image/x-icon" href="https://cdn4.iconfinder.com/data/icons/logos-3/504/php-512.png">
 	</head>
-	<style>
-		  p
-		{
-		  color: red;
-		  font-family: 3em;
-		}
-   </style>
    </head>
 	<body>
 	  <div class="alert alert-info">
-    <strong>Info!</strong>
+	  <img src="../img/me.png">
+	  <br>
+    <h3><strong>Información!</strong></h3>
     	<?php 
 	if (isset($_REQUEST["Mensaje"])) {
 	  
 	  $mensaje=$_REQUEST["Mensaje"];
-	  echo "<p>$mensaje</p>";
+	  echo "<h5>$mensaje</h5>";
 	}
  ?>
   </div>
